0750b8eabb6c391b6b51eb6f2e3da8f3f5f5ea79
Mark Oliver <Mark Oliver@Talus.chmsutalisay.net>
2025-11-19 17:11:03 +0800

    tester

--- app/src/apollo/client.ts
+++ app/src/apollo/client.ts
@@ -3,8 +3,8 @@ import { errorLink } from "./errorHandling";
 import { cache } from "./cacheConfig";
 
 const httpLink = new HttpLink({
-  // uri: "http://192.168.156.105:4000/graphql",
-  uri: "http://localhost:4000/graphql",
+  uri: "http://192.168.156.105:4000/graphql",
+  // uri: "http://localhost:4000/graphql",
   credentials: "include",
 });
 
